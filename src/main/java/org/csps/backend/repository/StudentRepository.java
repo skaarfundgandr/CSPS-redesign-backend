fe45f129c47fcbd1c236005f3fd29fdee7e25cea
roginand <roginandv@gmail.com>
2025-08-31 17:39:09 +0800

    feat(repository + controller): added repository and controller for student

--- src/main/java/org/csps/backend/repository/StudentRepository.java
+++ src/main/java/org/csps/backend/repository/StudentRepository.java
@@ -2,7 +2,8 @@ package org.csps.backend.repository;
 
 import org.csps.backend.domain.entities.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
-    boolean existsByUsername(String username);
 }
