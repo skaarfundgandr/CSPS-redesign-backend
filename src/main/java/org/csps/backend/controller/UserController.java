04dfaed93b075663fab8772a1cda6c3815a429f3
roginand <roginandv@gmail.com>
2025-09-13 13:40:02 +0800

    feat(controller): added student controller

--- src/main/java/org/csps/backend/controller/UserController.java
+++ src/main/java/org/csps/backend/controller/UserController.java
@@ -2,13 +2,10 @@ package org.csps.backend.controller;
 
 import java.util.List;
 
-import org.csps.backend.domain.dtos.request.UserRequestDTO;
 import org.csps.backend.domain.dtos.response.UserResponseDTO;
 import org.csps.backend.service.UserService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
